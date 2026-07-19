8bf0218b52876f05f9d87b7536fa8a68f51ee21f
jc691668-a11y <jc691668@gmail.com>
2026-07-19 16:03:13 -0700

    Increase minimum height of top section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function HomePage() {
       <Header />
 
       <main>
-     <Section id="top" className="relative overflow-hidden bg-white min-h-[380px] lg:min-h-[700px]">
+     <Section id="top" className="relative overflow-hidden bg-white min-h-[480px] lg:min-h-[700px]">
 
   {/* Hero artwork */}
   <Image
